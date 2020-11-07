44392a292ec53cf34ad9e314e8d1bbc51c2f1847
IanCode09 <ianlombu@protonmail.com>
2020-11-08 00:49:36 +0700

    Hero Component

---
+++ frontend/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroImage from '../assets/images/hero.jpg'
+import { Image } from 'react-bootstrap'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <Image src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_Tallhero_Dash_en_US_1x._CB418727898_.jpg' />
+        </div>
+    )
+}
+
+export default Hero
