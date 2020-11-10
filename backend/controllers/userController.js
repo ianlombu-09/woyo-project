1e9b0c05339194fed22a622bfd31e786f398b4fb
IanCode09 <ianlombu@protonmail.com>
2020-11-10 12:19:01 +0700

    User Registration & Password Hash

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,7 @@ const authUser = asyncHandler(async(req, res) => {
 
 // @desc    Get User Profile
 // @route   GET /api/users/profil
-// @access  Privet
+// @access  Private
 const getUserProfile = asyncHandler(async(req, res) => {
     const user = await User.findById(req.user._id)
 
@@ -46,4 +46,40 @@ const getUserProfile = asyncHandler(async(req, res) => {
     
 })
 
-export { authUser, getUserProfile }
+
+// @desc    Register a New User
+// @route   POST /api/users/
+// @access  Public
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    const existUser = await User.findOne({ email })
+
+    if(existUser) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user Data')
+    }
+    
+})
+
+export { authUser, getUserProfile, registerUser }
