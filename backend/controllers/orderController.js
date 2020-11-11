68f784a77774e3f5e1e503afc5bafaee923a70b6
IanCode09 <ianlombu@protonmail.com>
2020-11-11 22:41:18 +0700

    Order Controller & Routes

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,39 @@
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+
+// @desc    Create New Order
+// @route   POST /api/orders
+// @access  Privet
+const addOrderItems = asyncHandler(async(req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice
+    } = req.body
+
+    if(orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error('No order items')
+    } else {
+        const order = new Order({
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice
+        })
+
+        const createOrder = await order.save()
+
+        res.status(201).json(createOrder)
+    }
+})
+
+export { addOrderItems }
