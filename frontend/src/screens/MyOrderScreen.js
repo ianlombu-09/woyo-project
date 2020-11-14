254ab9aa2ea3d9e2e6cc35d9eba2c56914a2e0fa
IanCode09 <ianlombu@protonmail.com>
2020-11-14 21:35:30 +0700

    User details & Orders reset

--- frontend/src/screens/MyOrderScreen.js
+++ frontend/src/screens/MyOrderScreen.js
@@ -6,7 +6,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listMyOrders } from '../actions/orderActions'
 
-const MyOrderScreen = () => {
+const MyOrderScreen = ({ history }) => {
 
     const dispatch = useDispatch()
 
@@ -17,59 +17,63 @@ const MyOrderScreen = () => {
     const { loading, error, orders} = orderListMy
 
     useEffect(() => {
-        dispatch(listMyOrders())
-    }, [dispatch])
+        if(!userInfo) {
+            history.push('/login')
+        } else {
+            dispatch(listMyOrders())
+        }
+        
+    }, [dispatch, history, userInfo])
 
     return (
         <div className='myorderscreen__container'>
-            <h4 style={{ marginBottom: '30px' }}>Hey, {userInfo.name}</h4>
-            <h5 
-                className='text-light'
-                style={{ marginBottom: '20px' }}
-            >
-                My Orders
-            </h5>
-
             {loading ? (
                 <Loader />
             ): error ? (
                 <Message variant='danger'>{error}</Message>
             ): (
-                <Table responsive="sm">
-                    <thead>
-                        <tr>
-                            <th>NO</th>
-                            <th>ORDER ID</th>
-                            <th>DATE</th>
-                            <th>TOTAL</th>
-                            <th>PAID</th>
-                            <th>DELIVERED</th>
-                            <th>ACTION</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orders.map((order) => (
-                            <tr key={order._id}>
-                                <td>1</td>
-                                <td>{order._id}</td>
-                                <td>{order.createdAt.substring(0, 10)}</td>
-                                <td>${order.totalPrice}</td>
-                                <td style={{ color: 'red'}}>
-                                    {order.isPaid ? order.paidAt.substring(0, 10) : 'Not Paid'}
-                                </td>
-                                <td style={{ color: 'red'}}>
-                                    {order.isDelivered ? order.deliveredAt.substring(0, 10) : 'Not Delivered'}
-                                </td>
-                                <td>
-                                    <LinkContainer to={`/order/${order._id}`}>
-                                        <Button variant='info'>Details</Button>
-                                    </LinkContainer>
-                                </td>
+                <>
+                    {/* <h4 style={{ marginBottom: '30px' }}>Hey, {userInfo.name}</h4> */}
+                    <h5 
+                        className='text-light'
+                        style={{ marginBottom: '20px' }}
+                    >
+                        My Orders
+                    </h5>
+                    <Table responsive="sm">
+                        <thead>
+                            <tr>
+                                <th>ORDER ID</th>
+                                <th>DATE</th>
+                                <th>TOTAL</th>
+                                <th>PAID</th>
+                                <th>DELIVERED</th>
+                                <th>ACTION</th>
                             </tr>
-                        ))}
-                        
-                    </tbody>
-                </Table>
+                        </thead>
+                        <tbody>
+                            {orders.map((order) => (
+                                <tr key={order._id}>
+                                    <td>{order._id}</td>
+                                    <td>{order.createdAt.substring(0, 10)}</td>
+                                    <td>${order.totalPrice}</td>
+                                    <td style={{ color: 'red'}}>
+                                        {order.isPaid ? order.paidAt.substring(0, 10) : 'Not Paid'}
+                                    </td>
+                                    <td style={{ color: 'red'}}>
+                                        {order.isDelivered ? order.deliveredAt.substring(0, 10) : 'Not Delivered'}
+                                    </td>
+                                    <td>
+                                        <LinkContainer to={`/order/${order._id}`}>
+                                            <Button variant='info'>Details</Button>
+                                        </LinkContainer>
+                                    </td>
+                                </tr>
+                            ))}
+                            
+                        </tbody>
+                    </Table>
+                </>
             )}
         </div>
     )
